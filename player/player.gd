4485f0c04e63f7d1be5b5e928e0489d9348da1f0
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-10-03 15:06:12 -0300

    feat: adds a simple state machine for player character

--- player/player.gd
+++ player/player.gd
@@ -5,38 +5,52 @@ const JUMP_VELOCITY = -400.0
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var direction
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		sprite.play("jumping")
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("left", "right")
+	direction = Input.get_axis("left", "right")
 	
 	if direction:
 		if Input.is_action_pressed("run"):
 			velocity.x = direction * (SPEED * 2)
-			sprite.play("walking", 2)
+		elif Input.is_action_pressed("roll"):
+			velocity.x = direction * (SPEED * 4)
 		else:
 			velocity.x = direction * SPEED
-			sprite.play("walking")
-
-		set_face_direction(direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		sprite.play("idle")
-		
-	#if Input.is_action_just_pressed("run"):
-		#print("is_action_just_pressed: RUN!")
-	#if Input.is_action_pressed("run"):
-		#print("is_action_pressed: RUN!")
 
+	set_state()
 	move_and_slide()
 
-func set_face_direction(direction):
-	if direction == -1:
+func set_face_direction():
+	if direction < 0:
 		sprite.flip_h = true
-	else:
+	elif direction > 0:
 		sprite.flip_h = false
+
+func set_state():
+	set_face_direction()
+	
+	var state
+	
+	if is_on_floor():
+		if Input.is_action_pressed("roll"):
+			state = "rolling"
+		elif velocity.x == 0:
+			state = "idle"
+		else:
+			state = "walking"
+	elif !is_on_floor():
+		if velocity.y < 0:
+			state = "jumping"
+		else:
+			state = "falling"
+
+	sprite.play(state)
