0a3f2ce646b2f5fe723f09f024c1f8a8d8b0f198
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-10-06 18:35:40 -0300

    feat: adds patrol enemy

---
+++ enemies/spike_patrol.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const SPEED = 700.0
+
+@onready var sprite: Sprite2D = $Sprite
+@onready var ray_cast: RayCast2D = $RayCast
+
+var direction: int = -1
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if ray_cast.is_colliding():
+		direction *= -1
+		ray_cast.scale.x *= -1
+	
+	if direction == 1:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+
+	velocity.x = direction * SPEED * delta
+
+	move_and_slide()
