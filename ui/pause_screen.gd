521d80f918dca00d13b7ad7b11f996e56604fe22
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-10-07 19:16:31 -0300

    refactor: change pause menu to a new scene

--- ui/pause_screen.gd
+++ ui/pause_screen.gd
@@ -1,16 +1,26 @@
-extends Panel
+extends CanvasLayer
 
-@onready var pause_screen: Panel = $"."
+@onready var resume_button: TextureButton = $ButtonsContainer/ResumeButton
+@onready var menu_button: TextureButton = $ButtonsContainer/MenuButton
+@onready var credits_button: TextureButton = $ButtonsContainer/CreditsButton
+@onready var quit_button: TextureButton = $ButtonsContainer/QuitButton
 
-func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("pause")):
+func _ready():
+	visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
 		get_tree().paused = true
-		pause_screen.show()
+		visible = true
+		resume_button.grab_focus()
 
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
-	pause_screen.hide()
+	visible = false
 
 func _on_menu_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://ui/start_screen.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
