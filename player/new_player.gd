f9c24ebc2630dd3db4f7ac31e066e3662b92763a
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-10-09 15:10:54 -0300

    feat: adds parallax background

--- player/new_player.gd
+++ player/new_player.gd
@@ -1,55 +1,65 @@
 extends CharacterBody2D
 
-enum STATE {
-	IDLE,
-	WALKING,
-	RUNNING,
-	JUMPING,
-	FALLING,
-	KICKING,
-	ROLLING,
-}
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var is_idle := false
+var is_walking := false
+var is_jumping := false
+var is_falling := false
+
 func _physics_process(delta: float) -> void:
+	print("is_idle: ", is_idle)
+	print("is_walking: ", is_walking)
+	print("is_jumping: ", is_jumping)
+	print("is_falling: ", is_falling)
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		animation_player.play("jumping")
-	else:
-		animation_player.play("idle")
+		is_idle = false
+		is_walking = false
+		
+		if velocity.y < 0:
+			print("Pulando")
+			is_jumping = true
+			is_falling = false
+		else:
+			print("Caindo")
+			is_jumping = false
+			is_falling = true
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		jump()
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if is_on_floor():
+		if direction:
+			animation_player.play("walking")
+			velocity.x = direction * SPEED
+			is_idle = false
+			is_walking = true
+			is_jumping = false
+			is_falling = false
+		else:
+			animation_player.play("idle")
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			is_idle = true
+			is_walking = false
+			is_jumping = false
+			is_falling = false
 
 	move_and_slide()
+	set_state()
 
 func set_state() -> void:
-	pass
-
-func walk() -> void:
-	pass
-
-func run() -> void:
-	pass
-
-func jump() -> void:
-	velocity.y = JUMP_VELOCITY
-
-func fall() -> void:
-	pass
-
-func kick() -> void:
-	pass
-
-func roll() -> void:
-	pass
+	if is_idle:
+		animation_player.play("idle")
+	elif is_walking:
+		animation_player.play("walking")
+	elif is_jumping:
+		animation_player.play("jumping")
+	elif is_falling:
+		animation_player.play("falling")
