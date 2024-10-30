6d6deff02d98a630e68c6a1fa74194409fcfcbc7
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-10-29 21:20:55 -0300

    feat: inits first stage design

---
+++ levels/base_stage.gd
@@ -0,0 +1,30 @@
+extends Node
+
+class_name BaseStage
+
+const PLAYER_SCENE = preload("res://player/player.tscn")
+
+@export_category("Stage Elements")
+@export var hud: CanvasLayer
+@export var camera: Camera2D
+@export var player: CharacterBody2D
+
+func _ready() -> void:
+	Globals.player = player
+	Globals.player.camera_follow(camera)
+	Globals.player.has_died.connect(reload_level)
+	hud.time_is_up.connect(game_over)
+
+func reload_level():
+	await get_tree().create_timer(1.0).timeout
+	var player = PLAYER_SCENE.instantiate()
+	add_child(player)
+	Globals.player = player
+	Globals.player.camera_follow(camera)
+	Globals.player.has_died.connect(reload_level)
+	Globals.coins = 0
+	#Globals.lives = 3 # Rever regra
+	Globals.respawn_player()
+
+func game_over():
+	get_tree().reload_current_scene()
---
+++ levels/stage_1/stage_1.gd
@@ -0,0 +1,4 @@
+extends BaseStage
+
+func _ready() -> void:
+	super()
