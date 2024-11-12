71c43fe92a1efda7518f320f8abd781a8a85e5b7
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-11-12 20:33:15 -0300

    feat: implements camera movement between remotes in test level

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -22,37 +22,45 @@ var emited: bool = false
 func _ready() -> void:
 	Globals.player_start_position = start_position
 	Globals.player = player
-	Globals.player.camera_follow(camera)
+	#Globals.player.camera_follow(camera)
 	Globals.player.has_died.connect(reload_level)
 	hud.time_is_up.connect(game_over)
 	transition.material.set_shader_parameter("type", transition_type)
 	transition_animation.speed_scale = duration
 	diamond.connect("stage_cleared", clear)
-	self.connect("voltar_camera", voltar_camera_para_jogador)
+	#self.connect("voltar_camera", voltar_camera_para_jogador)
 
 func _process(delta: float) -> void:
 	#var shards_count = shards.get_child_count()
-	if (Globals.shards == 5 and is_instance_valid(diamond)):
-		Globals.player.camera_follow("")
-		var tween = create_tween()
-		tween.tween_property(camera, "position", marker_2d.position, 2.0)
-		await tween.finished
-
+	if Globals.shards < 5:
+		Globals.player.remote.remote_path = camera.get_path()
+	elif (Globals.shards == 5 and is_instance_valid(diamond)):
+		Globals.player.remote.remote_path = ""
+		diamond.remote.remote_path = camera.get_path()
+		#Globals.player.camera_follow("")
+		#var tween = create_tween()
+		#tween.tween_property(camera, "position", marker_2d.position, 2.0)
+		#await tween.finished
+#
+		await get_tree().create_timer(1.0).timeout
 		diamond.appear()
+		await get_tree().create_timer(1.0).timeout
 
-		await get_tree().create_timer(2.0).timeout
+		#diamond.remote.remote_path = ""
+		Globals.player.remote.remote_path = camera.get_path()
+		#await get_tree().create_timer(2.0).timeout
 		
 		#if not emited:
 			#emited = true
 			#emit_signal("voltar_camera")
-		Globals.player.camera_follow(camera)
+		#Globals.player.camera_follow(camera)
 
 func reload_level():
 	await get_tree().create_timer(1.0).timeout
 	var player = player_scene.instantiate()
 	add_child(player)
 	Globals.player = player
-	Globals.player.camera_follow(camera)
+	#Globals.player.camera_follow(camera)
 	Globals.player.has_died.connect(reload_level)
 	Globals.coins = 0
 	#Globals.lives = 3 # Rever regra
