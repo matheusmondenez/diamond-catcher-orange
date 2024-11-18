761c4b651d9444bf66e24270f6d048ef936a28e5
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-11-18 20:09:09 -0300

    fix: fixes the spawn diamond pause effect

--- levels/base_stage.gd
+++ levels/base_stage.gd
@@ -12,6 +12,9 @@ const PLAYER_SCENE = preload("res://player/player.tscn")
 @export var diamond: Area2D
 @export var next_stage: PackedScene
 
+var emitted: bool = false
+signal all_shards_collected
+
 func _ready() -> void:
 	Globals.player_start_position = start_position
 	Globals.player = player
@@ -19,27 +22,15 @@ func _ready() -> void:
 	hud.time_is_up.connect(game_over)
 	diamond.get_node("Collision").disabled = true
 	diamond.connect("stage_cleared", clear)
+	self.connect("all_shards_collected", test)
 
 func _physics_process(delta: float) -> void:
 	if Globals.shards < 5 and is_instance_valid(Globals.player):
 		Globals.player.remote.remote_path = camera.get_path()
 	elif Globals.shards == 5 and is_instance_valid(diamond):
-		get_tree().paused = true
-		#Globals.player.set_physics_process(false)
-		
-		Globals.player.remote.remote_path = ""
-		diamond.remote.remote_path = camera.get_path()
-		await get_tree().create_timer(1.0).timeout
-		spawn_diamond()
-		await get_tree().create_timer(1.0).timeout
-		Globals.player.remote.remote_path = camera.get_path()
-		
-		#Globals.player.set_physics_process(true)
-		get_tree().paused = false
-
-		#for node in get_tree().get_root().get_children():
-			#node.process_mode = Node.PROCESS_MODE_INHERIT
-		print("Unpaused")
+		if not emitted:
+			emit_signal("all_shards_collected")
+			emitted = true
 
 func reload_level():
 	await get_tree().create_timer(1.0).timeout
@@ -63,10 +54,21 @@ func clear() -> void:
 	get_tree().paused = false
 	Globals.shards = 0
 
-	if next_stage:
-		get_tree().change_scene_to_packed(next_stage)
+	#if next_stage:
+		#get_tree().change_scene_to_packed(next_stage)
 
 func spawn_diamond() -> void:
 	if is_instance_valid(diamond):
 		diamond.get_node("Collision").disabled = false
 		diamond.show()
+
+func test() -> void:
+	print("ALL SHARDS")
+	get_tree().paused = true
+	Globals.player.remote.remote_path = ""
+	diamond.remote.remote_path = camera.get_path()
+	await get_tree().create_timer(1.0).timeout
+	spawn_diamond()
+	await get_tree().create_timer(1.0).timeout
+	Globals.player.remote.remote_path = camera.get_path()
+	get_tree().paused = false
