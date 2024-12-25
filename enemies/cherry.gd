1bb9926283b9830ce143376eb42d45701e17cc09
Matheus Mondenez <matheus.paulino@kroton.com.br>
2024-12-25 19:21:51 -0300

    fix: fix cherry wall collision

--- enemies/cherry.gd
+++ enemies/cherry.gd
@@ -3,8 +3,10 @@ extends BaseEnemy
 func _ready() -> void:
 	damage_sfx = $DamageSFX
 	ray_cast = $WallDetector
+	sprite = $SpriteAnimation
 
 func _physics_process(delta: float) -> void:
 	animation.animation_finished.connect(kill_flying)
 	apply_gravity(delta)
 	move(delta)
+	flip_sprite()
